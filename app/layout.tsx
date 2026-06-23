c9645054c87468fb46ed891d9e089a4aa7d7a039
AthMove <athmovco@gmail.com>
2026-06-23 21:13:03 +0200

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import Navbar from "@/components/Navbar";
 export const metadata = {
   title: "ATHMOV",
   description: "Marketplace deportivo premium",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
